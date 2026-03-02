a210744d3ad6286c8d25c973c600d78c16e5b3cd
dofandhuy <doanhuy205@gmail.com>
2026-03-02 14:17:06 +0700

    Update lastest vesion

--- models/log/log_sync_receive_dot_thu.py
+++ models/log/log_sync_receive_dot_thu.py
@@ -4,7 +4,9 @@ from odoo import models, fields, api
 from datetime import datetime
 from odoo.exceptions import ValidationError
 from odoo import Command
+
 _logger = logging.getLogger(__name__)
+
 
 class LogSyncReceivePayment(models.Model):
     _name = 'log.sync.receive.payment'
@@ -39,36 +41,34 @@ class LogSyncReceivePayment(models.Model):
             date_payment = data.get('date_payment')
             ma_dv_raw = str(data.get('unit_code') or '').strip()
 
-            _logger.info("========== XỬ LÝ API: %s ==========", student_code)
-
             # 2. Tìm gốc dữ liệu (Phải có Sinh viên & Đơn vị thật mới tạo được phiếu)
             unit = self.env['res.business.unit'].sudo().search([('code', '=', ma_dv_raw)], limit=1)
             student = self.env['res.partner'].sudo().search([('ma_sinh_vien', '=', student_code)], limit=1)
-
-            # 3. CHIẾN THUẬT: TỰ TẠO DỮ LIỆU NẾU DB TRỐNG (Dành cho mã xyz)
-            if student_code == 'XYZ' and student and unit:
-                existing = self.env['hp.thanh.toan.sinh.vien'].sudo().search([
-                    ('partner_id', '=', student.id),
-                    ('ngay_thanh_toan', '=', date_payment)
-                ], limit=1)
-
-                if not existing:
-                    _logger.info(">>> DATABASE TRỐNG: Đang tự động tạo dữ liệu mẫu cho xyz...")
-                    # Tìm hoặc tạo nhanh 1 đợt thu để gắn vào chi tiết
-                    dot_thu = self.env['hp.ql.dot.thu'].sudo().search([], limit=1)
-                    if not dot_thu:
-                        dot_thu = self.env['hp.ql.dot.thu'].sudo().create({'name': 'Kỳ hè 2025', 'code': 'SUMMER_2025'})
-
-                    self.env['hp.thanh.toan.sinh.vien'].sudo().create({
-                        'partner_id': student.id,
-                        'unit_id': unit.id,
-                        'ngay_thanh_toan': date_payment,
-                        'ct_tt_ids': [(0, 0, {
-                            'dot_thu_id': dot_thu.id,
-                            'product_id': self.env['product.product'].sudo().search([], limit=1).id,
-                            'so_tien': 9999999  
-                        })]
-                    })
+            # _logger.info("========== XỬ LÝ API: %s ==========", student_code)
+            # # 3. CHIẾN THUẬT: TỰ TẠO DỮ LIỆU NẾU DB TRỐNG (Dành cho mã xyz)
+            # if student_code == 'XYZ' and student and unit:
+            #     existing = self.env['hp.thanh.toan.sinh.vien'].sudo().search([
+            #         ('partner_id', '=', student.id),
+            #         ('ngay_thanh_toan', '=', date_payment)
+            #     ], limit=1)
+            #
+            #     if not existing:
+            #         _logger.info(">>> DATABASE TRỐNG: Đang tự động tạo dữ liệu mẫu cho xyz...")
+            #         # Tìm hoặc tạo nhanh 1 đợt thu để gắn vào chi tiết
+            #         dot_thu = self.env['hp.ql.dot.thu'].sudo().search([], limit=1)
+            #         if not dot_thu:
+            #             dot_thu = self.env['hp.ql.dot.thu'].sudo().create({'name': 'Kỳ hè 2025', 'code': 'SUMMER_2025'})
+            #
+            #         self.env['hp.thanh.toan.sinh.vien'].sudo().create({
+            #             'partner_id': student.id,
+            #             'unit_id': unit.id,
+            #             'ngay_thanh_toan': date_payment,
+            #             'ct_tt_ids': [(0, 0, {
+            #                 'dot_thu_id': dot_thu.id,
+            #                 'product_id': self.env['product.product'].sudo().search([], limit=1).id,
+            #                 'so_tien': 9999999
+            #             })]
+            #         })
 
             # 4. LOGIC SEARCH THẬT (Bốc dữ liệu từ DB trả về)
             if 'hp.thanh.toan.sinh.vien' in self.env.registry and student and unit:
@@ -90,11 +90,11 @@ class LogSyncReceivePayment(models.Model):
                         m_khoan = line.product_id.default_code or 'KHOAN_UNKNOWN'
                         if m_dot not in result_data["ct_tt_ids"]:
                             result_data["ct_tt_ids"][m_dot] = {}
-                        result_data["ct_tt_ids"][m_dot][m_khoan] = line.so_tien0
+                        result_data["ct_tt_ids"][m_dot][m_khoan] = line.so_tien
 
                     _logger.info(">>> THÀNH CÔNG: Đã bốc được dữ liệu từ Database.")
                     self.write({'state': 'done', 'date_done': datetime.now()})
-                    return '000', "Thành công (Dữ liệu thật)", result_data
+                    return '000', "Thành công ", result_data
 
             # 5. Nếu vẫn không thấy (do mã khác xyz hoặc thiếu partner/unit)
             return '147', "Không tìm thấy dữ liệu cho thông tin này", {}
